937273da53338db00a195e3f12bcf6ebf1d7b353
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-08-08 15:46:36 +0200

    stopped network calls on navigate back
    just had to make the inital network call in oncreate

--- app/src/main/java/com/skybox/seven/edustat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/skybox/seven/edustat/ui/home/HomeFragment.kt
@@ -15,23 +15,25 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeFragment : Fragment() {
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var binding: FragmentHomeBinding
+    private val homeController = HomeController()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.siteInfo.observe(this, Observer {
+            viewModel.getCourseList(it.userId)
+        })
+
+        viewModel.courseList.observe(this, Observer {
+            homeController.setData(false, it)
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        val homeController = HomeController()
         binding.recycler.setController(homeController)
-
-        viewModel.siteInfo.observe(viewLifecycleOwner, Observer {
-            viewModel.getCourseList(it.userId)
-        })
-
-        viewModel.courseList.observe(viewLifecycleOwner, Observer {
-            homeController.setData(false, it)
-        })
         return binding.root
     }
 }
